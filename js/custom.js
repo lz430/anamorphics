aec3996915ecadfeaee1675da41b426a47cbcf4d
Leonard <430designs@gmail.com>
2016-04-02 00:02:45 -0400

    Working on nav

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+  $('li.dropdown').addClass('open');
+  $('li.dropdown').on({
+    "shown.bs.dropdown": function() { $(this).data('closable', false); },
+    "click":             function() { $(this).data('closable', true);  },
+    "hide.bs.dropdown":  function() { return $(this).data('closable'); }
+  });
+});
+  
