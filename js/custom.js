9b67c87760c53a30af8fb8b885bdd05ac3bdfeee
Leonard <430designs@gmail.com>
2016-04-12 23:42:27 -0400

    Finished explore page

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,13 @@
-  $(document).ready(function(){
-  $('li.dropdown').addClass('open');
-  $('li.dropdown').on({
-    "shown.bs.dropdown": function() { $(this).data('closable', false); },
-    "click":             function() { $(this).data('closable', true);  },
-    "hide.bs.dropdown":  function() { return $(this).data('closable'); }
-  });
-});
+  $(document).ready(function(){
+  $('li.dropdown').addClass('open');
+  $('li.dropdown').on({
+    "shown.bs.dropdown": function() { $(this).data('closable', false); },
+    "click":             function() { $(this).data('closable', true);  },
+    "hide.bs.dropdown":  function() { return $(this).data('closable'); }
+  });
+
+  $('li.dropdown a').click(function(){
+    $(this).toggleClass('active');
+  });
+});
   
